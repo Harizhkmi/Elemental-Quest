a271d3742e2e2de95a11db961f4d5597e9a9ad52
HARIZ HAKIMI JASRI <hariz170906@gmail.com>
2026-03-27 02:16:02 +0800

    Merge Update

--- Elemental Quest/obj/Debug/net10.0/Elemental Quest.AssemblyInfo.cs
+++ Elemental Quest/obj/Debug/net10.0/Elemental Quest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Elemental Quest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0672d59b463903ee1fcd8a9adf30746e0fdceac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25402c4af2d5c446016c4b346941b02e4a876d7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Elemental Quest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Elemental Quest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
